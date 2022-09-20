87ef4a8b4eefcc5a9d75142ac9dc4b010e495b7f
Robson <robccorreia@gmail.com>
2022-09-20 19:01:38 -0300

    feat: insert, unset and remove user age

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,32 +1,70 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import { User } from "../models/User";
 
 export const nome = (req: Request, res: Response) => {
-    let nome: string = req.query.nome as string;
-    let idade: string = req.query.idade as string;
+  let nome: string = req.query.nome as string;
+  let idade: string = req.query.idade as string;
 
-    res.render('pages/nome', {
-        nome,
-        idade
-    });
+  res.render("pages/nome", {
+    nome,
+    idade,
+  });
 };
 
 export const idadeForm = (req: Request, res: Response) => {
-    res.render('pages/idade');
+  res.render("pages/idade");
 };
 
 export const idadeAction = (req: Request, res: Response) => {
-    let mostrarIdade: boolean = false;
-    let idade: number = 0;
+  let mostrarIdade: boolean = false;
+  let idade: number = 0;
 
-    if(req.body.ano) {
-        let anoNascimento: number = parseInt(req.body.ano as string);
-        let anoAtual: number = new Date().getFullYear();
-        idade = anoAtual - anoNascimento;
-        mostrarIdade = true;
-    }
+  if (req.body.ano) {
+    let anoNascimento: number = parseInt(req.body.ano as string);
+    let anoAtual: number = new Date().getFullYear();
+    idade = anoAtual - anoNascimento;
+    mostrarIdade = true;
+  }
 
-    res.render('pages/idade', {
-        idade,
-        mostrarIdade
-    });
+  res.render("pages/idade", {
+    idade,
+    mostrarIdade,
+  });
+};
+
+export const addIdade = async (req: Request, res: Response) => {
+  let id: string = req.params.id;
+
+  let results = await User.findAll({ where: { id } });
+  if (results.length > 0) {
+    let usuario = results[0];
+    usuario.age++;
+    await usuario.save();
+  }
+
+  res.redirect("/");
+};
+
+export const diminuirIdade = async (req: Request, res: Response) => {
+  let id: string = req.params.id;
+
+  let results = await User.findAll({ where: { id } });
+  if (results.length > 0) {
+    let usuario = results[0];
+    usuario.age--;
+    await usuario.save();
+  }
+
+  res.redirect("/");
+};
+export const excluir = async (req: Request, res: Response) => {
+  let id: string = req.params.id;
+
+  let results = await User.findAll({ where: { id } });
+  if (results.length > 0) {
+    let usuario = results[0];
+    await usuario.destroy();
+  }
+
+  res.redirect("/");
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,22 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import * as HomeController from '../controllers/homeController';
-import * as InfoController from '../controllers/infoController';
-import * as UserController from '../controllers/userController';
+import * as HomeController from "../controllers/homeController";
+import * as InfoController from "../controllers/infoController";
+import * as UserController from "../controllers/userController";
 
 const router = Router();
 
-router.get('/', HomeController.home);
-router.post('/novousuario', HomeController.novoUsuario);
+router.get("/", HomeController.home);
+router.post("/novousuario", HomeController.novoUsuario);
+router.get("/usuario/:id/mais", UserController.addIdade);
+router.get("/usuario/:id/menos", UserController.diminuirIdade);
+router.get("/usuario/:id/excluir", UserController.excluir);
 
-router.get('/contato', InfoController.contato);
-router.get('/sobre', InfoController.sobre);
+router.get("/contato", InfoController.contato);
+router.get("/sobre", InfoController.sobre);
 
-router.get('/nome', UserController.nome);
-router.get('/idade', UserController.idadeForm);
-router.post('/idade-resultado', UserController.idadeAction);
+router.get("/nome", UserController.nome);
+router.get("/idade", UserController.idadeForm);
+router.post("/idade-resultado", UserController.idadeAction);
 
 export default router;
